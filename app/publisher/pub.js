95d130c60f5008fad696d50bf24778c3fb0577b8
wilmer <rubiorojasw@gmail.com>
2020-06-16 13:34:43 -0500

    new metrics was added

--- app/publisher/pub.js
+++ app/publisher/pub.js
@@ -1,33 +1,123 @@
 const mqtt = require('mqtt');
+const weather = require('weather-js');
+const cronJob = require('cron').CronJob;
+const {BigQuery} = require('@google-cloud/bigquery');
+
+const bigquery = new BigQuery();
 const client = mqtt.connect('mqtt://localhost:1883');
-const topic = 'temperatureSensors';
-const SerialPort = require('serialport');
-const port = new SerialPort('/dev/cu.usbmodem14401', {
-  baudRate: 9600,
-});
+const topicA = 'ZonaA';
+const topicB = 'ZonaB';
+const topicC = 'ZonaC';
 
-let newValues = '';
+let temp;
+let devices = 20;
+let medellin = [];
+
+async function query() {
+  // Queries the U.S. given names dataset for the state of Texas.
+
+  const query = `SELECT *
+    FROM \`bigquery-public-data.openaq.global_air_quality\`
+    WHERE country = 'CO'
+    LIMIT 100`;
+
+  // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+  const options = {
+    query: query,
+    // Location must match that of the dataset(s) referenced in the query.
+    location: 'US',
+  };
+
+  // Run the query as a job
+  const [job] = await bigquery.createQueryJob(options);
+  console.log(`Job ${job.id} started.`);
+
+  // Wait for the query to finish
+  const [rows] = await job.getQueryResults();
+
+  // Print the results
+  console.log('Rows:');
+  medellin = rows.filter(row => row.city === 'Medellin' && row.pollutant === 'pm25');
+  medellin = medellin.map((zone, i) => {
+    zone['id'] = i + 1;
+    zone['device'] =  `ZN_A_Sensor_${i + 1}`;
+
+    return zone;
+  });
+  console.log(medellin);
+}
+
+query();
+// ================================================================
+// Get temperature of city selected
+function getCityWeather() {
+  weather.find({search: 'Bogota', degreeType: 'C'}, function(err, result) {
+    if(err) console.log(err);
+   
+    const data = JSON.parse(JSON.stringify(result, null, 2));
+    temp = +data[0].current.temperature;
+
+    console.log('TEMPERATURA ACTUAL: ', temp);
+  }, null, true, 'America/Bogota');
+}
 
 // ================================================================
-// Get Serial port conection status
-port.on('open', () => {
-  console.log('conection is open');
+// Create random values between +- the current temperature value obtained from the city
+function getRandomTemperature() {
+  const lowerTemp = temp - 1;
+  const higherTemp = temp + 1;
+  const randomTemp = (Math.random() * (higherTemp - lowerTemp) + lowerTemp).toFixed(2);
+
+  return randomTemp;
+}
+
+// ================================================================
+// Publishes the values of each device
+function publicTemperatureValues(devices, i) {
+  devices.forEach(device => {
+    if (i === 0) {
+      client.publish(topicB, JSON.stringify(device));
+    } else {
+      client.publish(topicC, JSON.stringify(device));
+    }
+  });
+
+  medellin.forEach(item => {
+    client.publish(topicA, JSON.stringify(item));
+  });
+}
+
+// ================================================================
+// Cron job that obtains the temperature of the selected city every 2 min
+const getCityWeatheJob = new cronJob('0 */30 * * * *', () => {
+  query();
+  getCityWeather();
 });
 
 // ================================================================
-// Create object message to send it
-port.on('data', (data) => {
-  const algo = data.toString();
-  newValues += algo;
-  if (algo.includes('}')) {
-    const message = JSON.parse(newValues);
-    client.publish(topic, JSON.stringify(message));
-    newValues = '';
+// Cron job that creates random temperature values for each device
+const setRandomTemperature = new cronJob('*/15 * * * * *' , () => {
+  const  devicesTemp = [];
+
+  for (let z = 0; z < 2; z++) {
+    for (let i = 0; i < devices; i++) {
+      devicesTemp[i] = {
+        id: (i + 1),
+        device: `ZN_${z === 0 ? 'B' : 'C'}_Sensor_${i + 1}`,
+        metric: 'Temperature',
+        unit: 'Celsius',
+        value: getRandomTemperature(),
+        zone: z === 0 ? 'Zone B' : 'Zone C'
+      };
+    }
+    publicTemperatureValues(devicesTemp, z);
   }
+
 });
 
 // ================================================================
-// Get errors on sending message
-port.on('error', (err) => {
-  console.log(err);
-});
+// starts jobs temperatures
+query();
+getCityWeather();
+getCityWeatheJob.start();
+setRandomTemperature.start();
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const mosca = require('mosca');
-const brokerPort = { port: 1883, };
-const brokerServer = new mosca.Server(brokerPort);
+const port = process.env.PORT || 3800;
+const brokerServer = new mosca.Server({
+  http: {
+    port: port,
+    bundle: true,
+    static: './',
+  }
+});
 
 // ================================================================
 // Start Publisher 
@@ -10,11 +16,15 @@ require('./app/publisher/pub');
 // ================================================================
 // Get broker connection status
 brokerServer.on('ready', () => {
-  console.log('Broker server is ready and is running on port: ', brokerPort.port );
+  console.log('Broker server is ready and is running on port: ', port);
 });
 
 // ================================================================
 // Get publisher message
 brokerServer.on('published', (packet) => {
-  console.log(packet.payload.toString());
+  // console.log(packet.payload.toString());
+});
+
+brokerServer.on('clientConnected', function(client) {
+  console.log('client connected', client.id);
 });
