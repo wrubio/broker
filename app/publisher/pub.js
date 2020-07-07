82a04ba2345b38e4089cc5fc1e38da68565d81c3
wilmer <rubiorojasw@gmail.com>
2020-07-07 04:17:29 -0500

    add img video to transfer

--- app/publisher/pub.js
+++ app/publisher/pub.js
@@ -1,16 +1,14 @@
 const mqtt = require('mqtt');
-const weather = require('weather-js');
+var fs = require('fs');
 const cronJob = require('cron').CronJob;
 const {BigQuery} = require('@google-cloud/bigquery');
 
 const bigquery = new BigQuery();
 const client = mqtt.connect('mqtt://localhost:1883');
 const topicA = 'ZonaA';
-const topicB = 'ZonaB';
-const topicC = 'ZonaC';
+const topicB = 'Image';
+const topicC = 'Video';
 
-let temp;
-let devices = 20;
 let medellin = [];
 
 async function query() {
@@ -38,48 +36,18 @@ async function query() {
   // Print the results
   medellin = rows.filter(row => row.city === 'Medellin' && row.pollutant === 'pm25');
   medellin = medellin.map((zone, i) => {
-    zone['id'] = i + 1;
-    zone['device'] =  `ZN_A_Sensor_${i + 1}`;
+    zone[`id`] = i + 1;
+    zone[`device`] =  `ZN_A_Sensor_${i + 1}`;
 
     return zone;
   });
 }
 
 query();
-// ================================================================
-// Get temperature of city selected
-function getCityWeather() {
-  weather.find({search: 'Bogota', degreeType: 'C'}, function(err, result) {
-    if(err) console.log(err);
-   
-    const data = JSON.parse(JSON.stringify(result, null, 2));
-    temp = +data[0].current.temperature;
-
-    console.log('TEMPERATURA ACTUAL: ', temp);
-  }, null, true, 'America/Bogota');
-}
-
-// ================================================================
-// Create random values between +- the current temperature value obtained from the city
-function getRandomTemperature() {
-  const lowerTemp = temp - 1;
-  const higherTemp = temp + 1;
-  const randomTemp = (Math.random() * (higherTemp - lowerTemp) + lowerTemp).toFixed(2);
-
-  return randomTemp;
-}
 
 // ================================================================
 // Publishes the values of each device
-function publicTemperatureValues(devices, i) {
-  devices.forEach(device => {
-    if (i === 0) {
-      client.publish(topicB, JSON.stringify(device));
-    } else {
-      client.publish(topicC, JSON.stringify(device));
-    }
-  });
-
+function publicTemperatureValues() {
   medellin.forEach(item => {
     client.publish(topicA, JSON.stringify(item));
   });
@@ -87,35 +55,43 @@ function publicTemperatureValues(devices, i) {
 
 // ================================================================
 // Cron job that obtains the temperature of the selected city every 2 min
-const getCityWeatheJob = new cronJob('0 */30 * * * *', () => {
+const getCityWeatheJob = new cronJob('*/30 * * * * *', () => {
   query();
-  getCityWeather();
+  publicTemperatureValues();
+  genPayload();
+  genVideo();
 });
 
 // ================================================================
-// Cron job that creates random temperature values for each device
-const setRandomTemperature = new cronJob('*/15 * * * * *' , () => {
-  const  devicesTemp = [];
+// Cron job that obtains the temperature of the selected city every 2 min
+function genPayload() {
+  const data = fs.readFileSync('kittens-small.jpeg');
+  const base64data = data.toString('base64');
 
-  for (let z = 0; z < 2; z++) {
-    for (let i = 0; i < devices; i++) {
-      devicesTemp[i] = {
-        id: (i + 1),
-        device: `ZN_${z === 0 ? 'B' : 'C'}_Sensor_${i + 1}`,
-        metric: 'Temperature',
-        unit: 'Celsius',
-        value: getRandomTemperature(),
-        zone: z === 0 ? 'Zone B' : 'Zone C'
-      };
-    }
-    publicTemperatureValues(devicesTemp, z);
-  }
+  const message = {
+    type: 'image',
+    format: 'jpeg',
+    data: base64data
+  };
 
-});
+  client.publish(topicB, JSON.stringify(message));
+}
 
+function genVideo() {
+  const data = fs.readFileSync('video.mp4');
+  const base64data = data.toString('base64');
+
+  const message = {
+    type: 'video',
+    format: 'mp4',
+    data: base64data
+  };
+
+  client.publish(topicC, JSON.stringify(message));
+}
 // ================================================================
 // starts jobs temperatures
+genPayload();
+genVideo();
 query();
-getCityWeather();
 getCityWeatheJob.start();
-setRandomTemperature.start();
